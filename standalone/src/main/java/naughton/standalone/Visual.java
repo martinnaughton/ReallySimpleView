8358e99df6c43f5ef48c66186fedb3dc58166d09
martin <blades2000@gmail.com>
2013-11-10 19:01:32 +0000

    Really simple view plugin just to understand how to register a custom view in Jenkins.

---
+++ standalone/src/main/java/naughton/standalone/Visual.java
@@ -0,0 +1,34 @@
+package naughton.standalone;
+
+import hudson.Extension;
+import hudson.model.AllView;
+import hudson.model.ViewDescriptor;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+
+//change the extension between ListView, AllView and View.See how the user interface changes.
+public class Visual extends AllView{
+	
+	@DataBoundConstructor
+	public Visual(String name) {
+		super(name);
+	}
+	
+	public String getType(){
+    	return "lager";
+    }
+	
+	@Extension
+    public static final class DescriptorImpl extends ViewDescriptor {
+            public DescriptorImpl() {
+                    super();
+            }
+
+            @Override
+            public String getDisplayName() {
+                    return "Visual";
+            }
+    }
+
+}
